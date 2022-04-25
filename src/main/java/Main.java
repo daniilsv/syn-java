15f3ce06e3513c42baf54417ac546e46c7b59efa
Daniil Samoilov <daniilsvin@gmail.com>
2022-04-25 19:52:39 +0300

    remove all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,8 @@
-import com.querydsl.jpa.impl.JPAQuery;
-import models.User;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        User user1 = new User();
-        user1.setName("change some code");
-
-        var entityManagerFactory = Persistence.createEntityManagerFactory("some");
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(user1);
-        em.getTransaction().commit();
-        em.close();
+        //
     }
 }
